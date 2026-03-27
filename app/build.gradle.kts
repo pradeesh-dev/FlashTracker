12153eb36725b58b3132598d516aa52ef48ca48f
I am Groot <pradeesh.analysis@gmail.com>
2026-03-27 11:59:53 +0530

    Add experimental Compose APIs to compiler arguments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,10 @@ android {
     }
     kotlinOptions {
         jvmTarget             = "17"
-        freeCompilerArgs     += listOf("-opt-in=androidx.compose.material3.ExperimentalMaterial3Api")
+        freeCompilerArgs     += listOf(
+        "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+        "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi"
+    )
     }
     buildFeatures {
         compose = true
